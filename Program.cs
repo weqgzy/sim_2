500e7502669a08fcbe92c7f08fe53d8e4b3ba7ec
SAVAGE <proj_1>
2023-04-04 18:15:29 +0300

    all_done

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿task();
+
+
+
+int ReadInt(string argument)
+{
+    Console.WriteLine("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void task()
+{
+	System.Console.WriteLine(GetSecondDigit(ReadInt ("number")));
+}
+
+
+int GetSecondDigit(int number)
+{
+  if (number < 10)
+  {
+    return -1;
+  }
+
+  while(number > 100)
+  {
+	number /=10;
+  }
+  return number % 10;
+}
