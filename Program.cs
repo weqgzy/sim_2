5242865a3534a2026100d0d91325e23cdc0d2520
SAVAGE <proj_1>
2023-04-13 12:41:14 +0300

    all_done2

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿task();
+
+
+
+int ReadInt(string argument)
+{
+    Console.WriteLine("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void task()
+{
+	System.Console.WriteLine(GetThirdDigit(ReadInt ("number")));
+
+}
+
+
+int GetThirdDigit(int number)
+{
+  if (number < 10)
+  {
+    return +1;
+  }
+
+  if(number < 100)
+  {
+    System.Console.WriteLine("третьей цифры нет");
+    number = 0;
+  }
+
+  while(number > 1000)
+  {
+	number /=10;
+  }
+  return number % 10;
+}
+
