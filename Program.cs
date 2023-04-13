d95654d099b1ae634faf88f0327920645f5c8b1c
SAVAGE <proj_1>
2023-04-13 12:45:51 +0300

    all_done3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿task();
+
+int ReadInt(string argument)
+{
+    System.Console.WriteLine("введите день недели");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void task()
+{
+	System.Console.WriteLine(GetHolidays(ReadInt ("number")));
+
+}
+
+int GetHolidays(int number)
+{
+  if (number <= 5)
+  {
+    System.Console.WriteLine("нет,это будни сэр");
+  }
+
+   else
+  {
+    System.Console.WriteLine("так точно сэр,выходные");
+  }
+
+  if (number > 7)
+  { 
+    System.Console.WriteLine($"в неделе не {number} дней");
+  }
+  return number;
+}
